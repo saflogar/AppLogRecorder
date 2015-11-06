e75db66d121e9dd9cac80f2c9e527a6588d2fdea
Sergio Flores <safgflores@hotmail.com>
2015-11-06 00:10:12 -0600

    first commit

---
+++ AutTest/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Data;
+
+
+namespace AutTest
+{
+    class Program
+    {
+        [DllImport("user32", EntryPoint = "FindWindow", CharSet = CharSet.Auto)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    
+
+        static void Main(string[] args)
+        {
+            try 
+            {
+                String path = "C:\\Users\\VBW7\\Documents\\Visual Studio 2013\\Projects\\WindowsFormsApplication1\\WindowsFormsApplication1\\bin\\Debug\\WindowsFormsApplication1.exe";
+                String connString = "Provider=Microsoft.Jet.OLEDB.4.0;" +
+"Data Source=C:\\Results\\results.xls;" +
+"Extended Properties=\"Excel 8.0;HDR=YES;IMEX=0\"";
+
+                Process p = Process.Start(path);
+
+                Reader r = new Reader(connString);
+
+                DataTable dt = r.Dt;
+
+                foreach (DataRow dr in dt.Rows)
+                {
+
+                }
+
+                IntPtr mwh = IntPtr.Zero;
+                bool formFound = false;
+                int attempts = 0;
+
+                while (!formFound && attempts < 50)
+                {
+                    if (mwh == IntPtr.Zero)
+                    {
+                        Console.WriteLine("Form not yet found");
+                        Thread.Sleep(100);
+                        ++attempts;
+                        mwh = FindWindow(null, "Form1");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Form has been found");
+                        formFound = true;
+                    }
+                }
+
+                if (mwh == IntPtr.Zero)
+                    throw new Exception("Could not find main window");
+            
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Fatal Error" +  ex.Message);
+            }
+           
+        }
+        }
+    }
+
---
+++ AutTest/Reader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace AutTest
+{
+    class Reader
+    {
+
+
+        private DataTable dt;
+
+        public DataTable Dt 
+        {
+            get { return dt; }
+        }
+
+       public Reader(String connString)
+        {
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = connString;
+            conn.Open();
+
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.CommandText = "SELECT * FROM tblResults";
+            cmd.Connection = conn;
+
+            DataTable dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables,null);
+
+            OleDbDataReader rdr = cmd.ExecuteReader();
+        }
+
+
+
+        
+
+
+    }
+}
